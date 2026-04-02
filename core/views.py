a722c35301dc542dd6c34cf1f56878840f992260
sitaramnandi <sitarampravashnandi@gmail.com>
2026-04-03 01:44:20 +0530

    second

--- core/views.py
+++ core/views.py
@@ -5,4 +5,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse("🚀 Django POC Running Successfully with cicd")
+    return HttpResponse("FINAL VERSION 🚀")
